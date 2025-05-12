aed8e7aa8b4afdb906991c71faf8ca6408fd13c8
juan manuel <isaacesgaysupergay@gmail.com>
2025-05-11 20:35:33 -0500

    terminando el carrucel

--- main.js
+++ main.js
@@ -21,6 +21,36 @@ function handleMobileChange(e) {
   }
 }
 
+async function crearSkills(){
+  try{
+    const response = await fetch(`skills.json`);
+    const data = await response.json();
+   const skillsContainer = document.querySelector('.skills__main');
+      document.querySelectorAll('.skills__container-card').forEach(card => card.remove());
+
+    const skills = data.skills;
+    skills.forEach(skill => {
+      const div = document.createElement('div');
+      div.classList.add('skills__container-card');
+      div.innerHTML = `
+        <div class="skills__card">
+                    <img src=${skill.img} alt="html">
+                    <h3 class="skills__card--title">${skill.titulo}</h3>
+                    <p class="skills__card--text">${skill.descripcion}</p>
+                </div>
+      `;
+      skillsContainer.appendChild(div);
+    });
+      
+    
+  }
+  catch (error) {
+    console.error('Error al cargar el archivo JSON:', error);
+  }
+}
+crearSkills();
+
+
 // Ejecutar al cargar
 handleMobileChange(mediaQuery);
 
